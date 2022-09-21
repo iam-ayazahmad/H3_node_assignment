4bc8a68a526a582e95a606e63f2acea578603750
Ayaz Ahmad <97458144+iam-ayazahmad@users.noreply.github.com>
2022-09-21 21:08:30 +0530

    FINAL

--- index.js
+++ index.js
@@ -5,8 +5,42 @@ const mongoose=require("mongoose")
 const app=express()
 
 const ConnectDB=()=>{
-    mongoose.connect("mongodb+srvAyaz_AhmadAYAZisLUCKY@cluster0.sxbry.mongodb.netmyFirstDatabaseretryWrites=true&w=majority")
+    return mongoose.connect("mongodb+srv://Ayaz_Ahmad:AYAZisLUCKY@cluster0.sxbry.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 
 }
 
-const userSchema
+const userSchema=mongoose.Schema({
+    name:String,
+    price:Number
+})
+
+const Myuser=mongoose.model("user",userSchema)
+
+app.post("/users",async(res,req)=>{
+    try{
+
+        const user= await Myuser.create(req.body)
+
+        return res.status(201).send(user)
+
+    }
+    catch(err){
+
+        return res.status(500).send({err:err.message})
+
+    }
+})
+
+
+app.listen(5900,async()=>{
+    try{
+
+        await ConnectDB()
+    }
+    catch(err){
+
+
+        console.log(err)
+
+    }
+})
